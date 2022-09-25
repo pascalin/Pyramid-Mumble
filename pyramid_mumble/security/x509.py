ed220725f007acf3a810ce9e671731cbaf27d516
David Suarez Pascal <david@apuntia.com>
2022-09-25 16:09:13 -0500

    Modified certificate processing based on cryptography package docs

--- pyramid_mumble/security/x509.py
+++ pyramid_mumble/security/x509.py
@@ -1,88 +1,90 @@
-# Copyright 2018 Simon Davy
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+import datetime
+import os
+import tempfile
 
-# WARNING: the code in the gist generates self-signed certs, for the purposes of testing in development.
-# Do not use these certs in production, or You Will Have A Bad Time.
-#
-# Caveat emptor
-#
-
-from datetime import datetime, timedelta
-import ipaddress
+import cryptography
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def generate_selfsigned_cert(hostname, ip_addresses=None, key=None):
-    """Generates self signed certificate for a hostname, and optional IP addresses."""
-    from cryptography import x509
-    from cryptography.x509.oid import NameOID
-    from cryptography.hazmat.primitives import hashes
-    from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives import serialization
-    from cryptography.hazmat.primitives.asymmetric import rsa
+def get_user_cert_and_key(user, password=None, tmp_dir=None):
+    if not tmp_dir:
+        tmp_dir = tempfile.mkdtemp()
+    key_path = os.path.join(tmp_dir, f"{user.email}_key.pem")
+    if user.privkey:
+        try:
+            with open(user.privkey, "rb") as privkey:
+                if password:
+                    key = serialization.load_pem_private_key(privkey.read(), password)
+                    with open(key_path, "wb") as f:
+                        f.write(key.private_bytes(
+                            encoding=serialization.Encoding.PEM,
+                            format=serialization.PrivateFormat.TraditionalOpenSSL,
+                            encryption_algorithm=serialization.NoEncryption(),
+                        ))
+                    return (user.certificate, key_path)
+        except ValueError:
+            return None
+        except TypeError:
+            raise
+        return (user.certificate, user.privkey)
+
+
+def generate_selfsigned_cert(user, key=None, size=2048, password=None, dest_dir=None):
+    """Generates self-signed certificate for a hostname, and optional IP addresses."""
+
+    if not dest_dir:
+        dest_dir = tempfile.mkdtemp()
+
+    crt_path = os.path.join(dest_dir, f"{user.email}_cert.pem")
+    key_path = os.path.join(dest_dir, f"{user.email}_key.pem")
 
     # Generate our key
     if key is None:
         key = rsa.generate_private_key(
             public_exponent=65537,
-            key_size=2048,
-            backend=default_backend(),
+            key_size=size,
+            # backend=default_backend(),
         )
 
-    name = x509.Name([
-        x509.NameAttribute(NameOID.COMMON_NAME, hostname)
+    # Write our key to disk for safe keeping
+    if password:
+        encryption_algorithm = serialization.BestAvailableEncryption(password)
+    else:
+        encryption_algorithm = serialization.NoEncryption()
+    with open(key_path, "wb") as f:
+        f.write(key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=encryption_algorithm,
+        ))
+
+    subject = issuer = cryptography.x509.Name([
+        cryptography.x509.NameAttribute(NameOID.COUNTRY_NAME, user.country),
+        cryptography.x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, user.state),
+        cryptography.x509.NameAttribute(NameOID.LOCALITY_NAME, ''),
+        cryptography.x509.NameAttribute(NameOID.ORGANIZATION_NAME, user.organization),
+        cryptography.x509.NameAttribute(NameOID.COMMON_NAME, user.realname),
     ])
 
-    # best practice seem to be to include the hostname in the SAN, which *SHOULD* mean COMMON_NAME is ignored.
-    alt_names = [x509.DNSName(hostname)]
+    cert = cryptography.x509.CertificateBuilder().subject_name(subject).issuer_name(issuer).public_key(key.public_key()).serial_number(cryptography.x509.random_serial_number()).not_valid_before(datetime.datetime.utcnow()).not_valid_after(
+    # Our certificate will be valid for 10 years
+    datetime.datetime.utcnow() + datetime.timedelta(days=10*365)
+    ).add_extension(
+        cryptography.x509.SubjectAlternativeName([
+            #x509.DNSName(u"localhost"),
+            # cryptography.x509.NameAttribute(NameOID.EMAIL_ADDRESS, user.email),
+            cryptography.x509.RFC822Name(user.email)
+        ]),
+        critical=False,
+    # Sign our certificate with our private key
+    ).sign(key, hashes.SHA256())
 
-    # allow addressing by IP, for when you don't have real DNS (common in most testing scenarios
-    if ip_addresses:
-        for addr in ip_addresses:
-            # openssl wants DNSnames for ips...
-            alt_names.append(x509.DNSName(addr))
-            # ... whereas golang's crypto/tls is stricter, and needs IPAddresses
-            # note: older versions of cryptography do not understand ip_address objects
-            alt_names.append(x509.IPAddress(ipaddress.ip_address(addr)))
+    # Write our certificate out to disk.
+    with open(crt_path, "wb") as f:
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
 
-    san = x509.SubjectAlternativeName(alt_names)
-
-    # path_len=0 means this cert can only sign itself, not other certs.
-    basic_contraints = x509.BasicConstraints(ca=True, path_length=0)
-    now = datetime.utcnow()
-    cert = (
-        x509.CertificateBuilder()
-        .subject_name(name)
-        .issuer_name(name)
-        .public_key(key.public_key())
-        .serial_number(1000)
-        .not_valid_before(now)
-        .not_valid_after(now + timedelta(days=10 * 365))
-        .add_extension(basic_contraints, False)
-        .add_extension(san, False)
-        .sign(key, hashes.SHA256(), default_backend())
-    )
-    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
-    key_pem = key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
-
-    return cert_pem, key_pem
+    return (crt_path, key_path)
